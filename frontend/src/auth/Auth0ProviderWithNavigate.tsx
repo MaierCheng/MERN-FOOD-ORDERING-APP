e00cef58b47f781233ea11535b9dc2b887a2e691
MaierCheng <cheng.yuha@northeastern.edu>
2026-02-21 14:39:02 -0500

    auth0 access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,12 +1,12 @@
 import { Auth0Provider, User, type AppState } from "@auth0/auth0-react";
-import { useCreateMyUser } from "../api/MyUserApi";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
     children: React.ReactNode;
 }
 
 const Auth0ProviderWithNavigate= ({ children }: Props) => {
-    const {createUser} = useCreateMyUser();
+    const navigate = useNavigate();
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_REDIRECT_URL;
@@ -16,13 +16,8 @@ const Auth0ProviderWithNavigate= ({ children }: Props) => {
     }
 
     const onRedirectCallback = (appState?: AppState,user?:User) => {
-        if(user?.sub && user?.email){
-            createUser({
-                auth0Id: user.sub,
-                email: user.email
-            });
+        navigate("/auth-callback");
         }
-    }
 
     return (<Auth0Provider
         domain={domain}
