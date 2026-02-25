359a98df814519db25df5e0beda367cc2fb16721
MaierCheng <cheng.yuha@northeastern.edu>
2026-02-24 21:27:09 -0500

    create toast notification

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -77,7 +78,9 @@ export const useUpdateMyUser = () => {
     isSuccess,
     error,
     reset,
-  } = useMutation(updateMyUserRequest);
+  } = useMutation<unknown, Error, UpdateMyUserRequest>({
+    mutationFn: updateMyUserRequest,
+  });
 
   if (isSuccess) {
     toast.success("User profile updated!");
@@ -87,6 +90,5 @@ export const useUpdateMyUser = () => {
     toast.error(error.toString());
     reset();
   }
-
-  return { updateUser, isPending };
+  return { updateUser, isPending};
 }
