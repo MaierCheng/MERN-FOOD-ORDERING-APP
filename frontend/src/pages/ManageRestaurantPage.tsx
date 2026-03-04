5097322948877ebb6ccfbefc99b9b159bb2ab51e
MaierCheng <cheng.yuha@northeastern.edu>
2026-03-03 22:57:15 -0500

    Create restaurant API request

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,8 +1,10 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restraurant-form/ManageRestaurantForm";
 
 
 const ManageRestaurantPage = () => {
-    return <ManageRestaurantForm onSave={() => {}} isPending={false} />;
+    const { createRestaurant, isPending } = useCreateMyRestaurant();
+    return <ManageRestaurantForm onSave={createRestaurant} isPending={isPending} />;
 };
 
 export default ManageRestaurantPage;
