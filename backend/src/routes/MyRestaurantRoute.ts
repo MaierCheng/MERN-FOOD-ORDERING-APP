2a98486b088e3680356f24b5bccfb0bc046fbd7f
MaierCheng <cheng.yuha@northeastern.edu>
2026-03-04 19:50:29 -0500

    Update my restaurant API

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -27,4 +27,14 @@ router.post(
   jwtParse,
   MyRestaurantController.createMyRestaurant
 );
+
+router.put(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.updateMyRestaurant
+);
+
 export default router;
