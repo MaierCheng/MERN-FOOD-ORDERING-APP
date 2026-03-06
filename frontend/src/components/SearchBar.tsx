fe8bf70993b2f76dcfa91d63c83fc4ea92fe1967
MaierCheng <cheng.yuha@northeastern.edu>
2026-03-06 00:13:27 -0500

    implement search filter function

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Search } from "lucide-react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   searchQuery: z.string().min(1, "Restaurant name is required"),
@@ -27,9 +28,9 @@ const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
     },
   });
 
-//   useEffect(() => {
-//     form.reset({ searchQuery });
-//   }, [form, searchQuery]);
+  useEffect(() => {
+    form.reset({ searchQuery });
+  }, [form, searchQuery]);
 
   const handleReset = () => {
     form.reset({
@@ -69,10 +70,7 @@ const SearchBar = ({ onSubmit, onReset, placeHolder, searchQuery }: Props) => {
             </FormItem>
           )}
         />
-        {form.formState.isDirty && (
-            <Button onClick={handleReset} type="button" variant="outline" className="rounded-full">Clear</Button>
-        )}
-
+        <Button onClick={handleReset} type="button" variant="outline" className="rounded-full">Reset</Button>
         <Button type="submit" className="rounded-full bg-orange-500">
           Search
         </Button>
