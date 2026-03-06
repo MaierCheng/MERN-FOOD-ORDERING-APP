eaa1ed1fd1d9e56f0707a82aaf0221a79078e8d7
MaierCheng <cheng.yuha@northeastern.edu>
2026-03-06 12:25:18 -0500

    implement pagination function

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,5 @@
 import { useSearchRestaurants } from "@/api/RestaurantApi";
+import PaginationSelector from "@/components/PaginationSelector";
 import SearchBar, { type SearchForm } from "@/components/SearchBar";
 import SearchResultCard from "@/components/SearchResultCard";
 import SearchResultInfo from "@/components/SearchResultInfo";
@@ -7,14 +8,22 @@ import { useParams } from "react-router-dom";
 
 export type SearchState = {
   searchQuery: string;
+  page:number;
 };
 
 const SearchPage = () => {
   const { city } = useParams();
   const [searchState, setSearchState] = useState<SearchState>({
     searchQuery: "",
+    page: 1,
   });
   const { results, isPending } = useSearchRestaurants(searchState,city);
+
+  const setPage = (page: number) => {
+    setSearchState((prevState) => ({
+      ...prevState, page,
+    }));
+  }
 
   const setSearchQuery = (searchFormData:SearchForm) => {
     setSearchState((prevState) => ({
@@ -47,6 +56,11 @@ const SearchPage = () => {
         {results.data.map((restaurant) => (
           <SearchResultCard  restaurant={restaurant} />
         ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
        </div>
     </div>
     
