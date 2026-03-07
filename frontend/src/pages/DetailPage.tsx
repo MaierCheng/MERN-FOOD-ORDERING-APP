85034d32378f7cc2eaf7e1b39053e63115bd5689
MaierCheng <cheng.yuha@northeastern.edu>
2026-03-07 14:17:48 -0500

    implement checkout button

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -3,10 +3,11 @@ import MenuItem from "@/components/MenuItem";
 import OrderSummary from "@/components/OrderSummary";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Card } from "@/components/ui/card";
+import { Card, CardFooter } from "@/components/ui/card";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import type { MenuItem as MenuItemType } from "../types";
+import CheckoutButton from "@/components/CheckoutButton";
 
 export type CartItem = {
   _id: string;
@@ -19,7 +20,10 @@ const DetailPage = () => {
     const { restaurantId } = useParams();
     const { restaurant, isPending } = useGetRestaurant(restaurantId);
 
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+    const [cartItems, setCartItems] = useState<CartItem[]>(()=>{
+      const storedCartItems = sessionStorage.getItem(`cartItems-${restaurantId}`);
+      return storedCartItems ? JSON.parse(storedCartItems) : [];
+    });
 
     const addToCart = (menuItem: MenuItemType) => {
     setCartItems((prevCartItems) => {
@@ -47,10 +51,10 @@ const DetailPage = () => {
         ];
       }
 
-    //   sessionStorage.setItem(
-    //     `cartItems-${restaurantId}`,
-    //     JSON.stringify(updatedCartItems)
-    //   );
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
 
       return updatedCartItems;
     });
@@ -62,10 +66,10 @@ const DetailPage = () => {
         (item) => cartItem._id !== item._id
       );
 
-    //   sessionStorage.setItem(
-    //     `cartItems-${restaurantId}`,
-    //     JSON.stringify(updatedCartItems)
-    //   );
+      sessionStorage.setItem(
+        `cartItems-${restaurantId}`,
+        JSON.stringify(updatedCartItems)
+      );
 
       return updatedCartItems;
     });
@@ -103,6 +107,9 @@ const DetailPage = () => {
               cartItems={cartItems}
               removeFromCart={removeFromCart}
             />
+            <CardFooter>
+              <CheckoutButton />
+            </CardFooter>
           </Card>
         </div>
       </div>
